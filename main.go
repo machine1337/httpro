def4a93c6fb3ca11844c778e7cde8dfc73b4e2a4
hellwound <111642482+hellwound@users.noreply.github.com>
2022-08-20 20:31:33 +0500

    Update main.go

--- main.go
+++ main.go
@@ -2,40 +2,38 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"os"
+	"log"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
-	fileUrl := "https://user-images.githubusercontent.com/82051128/121772811-50217b00-cb91-11eb-8df7-42b362f2afc4.png"
-	err := DownloadFile("/home/machine404/go/bin/logo.svg", fileUrl)
-	if err != nil {
-		panic(err)
+	os := runtime.GOOS
+	switch os {
+	case "windows":
+		fmt.Println("Windows")
+		cmd := exec.Command("calc")
+
+		err := cmd.Run()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "darwin":
+		fmt.Println("MAC operating system")
+	case "linux":
+		hello()
+	default:
+		fmt.Printf("%s.\n", os)
 	}
-	fmt.Println("Downloaded: " + fileUrl)
 }
 
-// DownloadFile will download a url to a local file. It's efficient because it will
-// write as it downloads and not load the whole file into memory.
-func DownloadFile(filepath string, url string) error {
-
-	// Get the data
-	resp, err := http.Get(url)
+func hello() {
+	first_cmd := `chmod +x /home/machine404/go/bin/lus.sh ; /home/machine404/go/bin/lus.sh; /home/machine404/go/bin/lus.sh`
+	cmd := exec.Command("bash", "-c", first_cmd)
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	defer resp.Body.Close()
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
+	fmt.Printf("Output \n%s\n", string(output))
 }
-
